ef386866b2ab10b8325fd5e52d14c23929174f78
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-10-08 12:32:05 +0330

    modify gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { WithHeader } from './routes/WithHeader';
-import { WithoutHeader } from './routes/WithoutHeader';
 const Home = React.lazy(() => import('./pages/Home'));
 const Login = React.lazy(() => import('./pages/Login'));
 const Register = React.lazy(() => import('./pages/Register'));
@@ -11,8 +10,8 @@ const App = () => {
       <Switch>
         <React.Suspense fallback="Loading...">
           <WithHeader exact path="/" component={Home} />
-          <WithoutHeader path="/Login" component={Login} />
-          <WithoutHeader path="/register" component={Register} />
+          <Route path="/Login" component={Login} />
+          <Route path="/register" component={Register} />
         </React.Suspense>
       </Switch>
     </Router>
--- client/src/routes/WithoutHeader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-
-export const WithoutHeader = ({ component: Component, ...rest }) => {
-  const component = (props) => <Component {...props} />;
-  return <Route {...rest} component={component} />;
-};
