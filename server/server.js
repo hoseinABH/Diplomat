e635755a07f998637fc9e887a4614cb4e2bee14a
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-10-13 22:08:23 +0330

    error handling middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 dotenv.config();
 connectDB();
 
@@ -12,6 +14,8 @@ app.get('/', (req, res) => {
 });
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
