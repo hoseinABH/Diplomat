6d07a4671816184af127c51ce0c80d5026e002fe
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-10-26 17:29:58 +0330

    serverCode:getOrderById from server

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,13 +2,7 @@ import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
 const addOrderItems = asyncHandler(async (req, res) => {
-  const {
-    orderItems,
-    shippingAddress,
-    paymentMethod,
-    itemsPrice,
-    totalPrice,
-  } = req.body;
+  const { orderItems, shippingAddress, paymentMethod, totalPrice } = req.body;
 
   if (orderItems && orderItems.length === 0) {
     res.status(400);
@@ -27,5 +21,17 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await (await Order.findById(req.params.id)).populated(
+    'user',
+    'name email'
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('سفارشی پیدا نشد.');
+  }
+});
 
-export { addOrderItems };
+export { addOrderItems, getOrderById };
