05ce1654c3b3a2c5ca7e977776f40ba8be7eed67
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-10-26 15:44:43 +0330

    adding orderRoutes in server js and add order functionality

---
+++ server/controllers/orderController.js
@@ -0,0 +1,32 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('سفارشی یافت نشد.');
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
