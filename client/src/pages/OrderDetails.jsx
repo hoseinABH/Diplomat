2c74b7a10e6cb03f431ca32e2cb54c16edaead5d
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-10-27 12:29:37 +0330

    serverCode:updating paid for order

--- client/src/pages/OrderDetails.jsx
+++ client/src/pages/OrderDetails.jsx
@@ -5,6 +5,8 @@ import SummaryItem from '../components/SummaryItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOrderDetails } from '../redux/actions/orderActions';
+import { FaCheckCircle } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 const OrderDetails = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
@@ -29,7 +31,17 @@ const OrderDetails = () => {
           ) : (
             <div className="flex flex-col md:flex-row w-full pt-8 pb-24">
               <div className="flex flex-col space-y-6 w-full md:w-4/6">
-                <div className="flex flex-col justify-center space-y-3  items-start py-5 px-6">
+                <div className="flex flex-col justify-center space-y-3  items-start my-5 px-6">
+                  <h1>کد سفارش :{order._id}</h1>
+                  <h4 className="text-lg font-semibold">اطلاعات فردی</h4>
+                  <p>نام:{order.user.name}</p>
+                  <p>
+                    ایمیل
+                    <a href={`mailto:${order.user.email}`}>
+                      {' '}
+                      {order.user.email}
+                    </a>
+                  </p>
                   <h4 className="text-lg font-semibold">اطلاعات ارسال</h4>
                   <p>
                     آدرس:{order.shippingAddress.state} ,{' '}
@@ -38,7 +50,22 @@ const OrderDetails = () => {
                   </p>
                   <p>کدپستی:{order.shippingAddress.postalCode}</p>
                 </div>
-                <div className="flex flex-col justify-center space-y-3 items-start py-5 px-6">
+
+                {order.isDelivered ? (
+                  <div className="flex items-center px-6">
+                    <IconContext.Provider
+                      value={{ color: '#01923e', size: '18px' }}
+                    >
+                      <FaCheckCircle />
+                    </IconContext.Provider>
+                    <p className="m-1">
+                      تحویل داده شده در تاریخ{order.DeliveredAt}
+                    </p>
+                  </div>
+                ) : (
+                  <p className="text-red-600 px-6">تحویل داده نشده </p>
+                )}
+                <div className="flex flex-col justify-center space-y-3 items-start px-6">
                   <h4 className="text-lg font-semibold">نحوه پرداخت</h4>
                   <p>
                     از طریق:{' '}
@@ -46,6 +73,19 @@ const OrderDetails = () => {
                       .getItem('paymentMethod')
                       .replace(/['"]+/g, '')}
                   </p>
+
+                  {order.isPaid ? (
+                    <div className="flex items-center justify-center">
+                      <IconContext.Provider
+                        value={{ color: '#01923e', size: '18px' }}
+                      >
+                        <FaCheckCircle />
+                      </IconContext.Provider>
+                      <p className="m-1">پرداخت شده در{order.paidAt}</p>
+                    </div>
+                  ) : (
+                    <p className="text-red-600">پرداخت نشده </p>
+                  )}
                 </div>
                 <div className="flex flex-col justify-center items-start px-6">
                   <h4 className="text-lg font-semibold">سفارشات</h4>
@@ -59,7 +99,8 @@ const OrderDetails = () => {
                 )}
               </div>
               <div className="w-full md:w-2/6 relative left-auto top-0 mt-12 ">
-                <div className="flex flex-col px-4 py-5  bg-white m-auto  md:ml-2 md:mr-8 rounded-lg border border-gray-300">
+                <div className="flex flex-col px-4 py-5  bg-white m-auto  md:ml-2 md:mr-8 rounded-lg border-0 sm:border border-gray-300">
+                  خلاصه سفارش
                   <div className="flex items-center mt-5 border justify-between border-gray-300 py-4 border-l-0 border-r-0 text-sm text-gray-900">
                     <p>جمع</p>
                     <p> {order.totalPrice} تومان</p>
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -34,5 +34,25 @@ const getOrderById = asyncHandler(async (req, res) => {
     throw new Error('سفارشی پیدا نشد.');
   }
 });
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
 
-export { addOrderItems, getOrderById };
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('سفارشی پیدا نشد.');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
