c22cbbd6ea39d33fd2e49fafc05a3c3cdfc10e0f
hoseinABH98 <hosein.abolhasani1998@gmail.com>
2020-12-25 12:13:41 +0330

    deploy

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import path from 'path';
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
@@ -19,18 +21,16 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
 
@@ -42,8 +42,10 @@ if (process.env.NODE_ENV === 'production') {
     res.send('API is running....');
   });
 }
+
 app.use(notFound);
 app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
